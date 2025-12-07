8f75fb37c441e5ff97c4b43bf3322eaa36a642f3
SHRAVYA MUNUGALA <100656699+Shravya29M@users.noreply.github.com>
2025-12-06 19:00:25 -0500

    i hate git

--- testing_triangular_mesh.py
+++ testing_triangular_mesh.py
@@ -1,5 +1,5 @@
 import open3d as o3d
-mesh = o3d.io.read_triangle_mesh("/Users/shravyamunugala/Documents/Github/Not-MeshGPT/exported_meshes_alpha/chair/chair_0902_recon.ply")
+mesh = o3d.io.read_triangle_mesh("/Users/shravyamunugala/Documents/Github/Not-MeshGPT/exported_meshes_alpha/chair/chair_0183_original.ply")
 o3d.visualization.draw_geometries([mesh], 
                                   mesh_show_wireframe=True,
                                   mesh_show_back_face=True)
