3255b6ba55de02c03fc8c5900442175e326f2ca1
delyc <delyce35@gmail.com>
2025-12-07 21:33:54 -0500

    fix: repalced  hardcoded paths to with relative paths

--- testing_triangular_mesh.py
+++ testing_triangular_mesh.py
@@ -1,5 +1,8 @@
 import open3d as o3d
-mesh = o3d.io.read_triangle_mesh("/Users/shravyamunugala/Documents/Github/Not-MeshGPT/exported_meshes_alpha/chair/chair_0183_original.ply")
+import os
+repo_root = os.path.dirname(os.path.abspath(__file__))  
+mesh_path = os.path.join(repo_root, "exported_meshes_alpha/chair/chair_0183_original.ply")
+mesh = o3d.io.read_triangle_mesh(mesh_path)
 o3d.visualization.draw_geometries([mesh], 
                                   mesh_show_wireframe=True,
                                   mesh_show_back_face=True)
